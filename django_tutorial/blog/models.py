48757b514edf954743af1ee09cfbf647b426f833
Parham Baghbanbashi <parham.baghbanbashi204@gmail.com>
2020-10-12 18:29:21 -0400

    use class based views to manipulate posts and figured out how to manipulate links

--- django_tutorial/blog/models.py
+++ django_tutorial/blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -15,3 +16,9 @@ class Post(models.Model):
     # dunder str method --> displays information when we print the class
     def __str__(self):
     	return self.title
+
+    # Redirect URL for class based views
+    # allows django to find the location of a specific post
+    # reverse displays url as string rederct redirects you to the view
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs=({'pk':self.pk}))
--- django_tutorial/blog/views.py
+++ django_tutorial/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import (ListView, 
+DetailView, 
+CreateView,
+UpdateView,
+DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 """
 def home(request):
@@ -10,6 +16,7 @@ def home(request):
 """
 
 
+# Function based view
 # homepage using templates
 def home(request):
     context = {
@@ -17,12 +24,80 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+# class based views
+# these class based views allow us to use djangos defult views and modify them
+class PostListView(ListView):
+    # we get these atributes form the ListViews MultipleObjectTemplateResponseMixin
+    # we can cut down on these atributes if we use the proper django conventions
+    model = Post
+    # template it uses
+    template_name = 'blog/home.html' # django template nameing convention: <app>/<model>_<viewtype>.html
+    # contex
+    context_object_name = 'posts'
+    # to allow us to display the newest post first we need to change the order of our qurery
+    # this attribute allows the backent to determine which atribute to order the posts by
+    ordering = ['-date_posted']# the minus sign orders the dates by newest to oldest
 
-"""
+
+# keeping to the conventions
+# looking at a single post
+class PostDetailView(DetailView):
+    model = Post
+    #looks for template blog/post_detail.html
+
+# creating posts
+# createview is a view with a form
+# class based views allow django to handle stuff in the background
+# mixens are added to the far left
+# MAKING IS SO THAT ONLY USERS CAN ACCSES THIS VIEW
+# MIXIN --> class that adds extra functionalty to the view
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    # we need to add feilds 
+    fields = ['title', 'content']
+    # for CreateView the convention is <model>_form.html
+
+    # assigns an author to the PostCreateView --> assigns author to new post
+    # reminder this class creates posts
+    # helps us avoid integraty errors
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    # if we want to go to the homepage insted of deataled view set sucsess_url to homepage
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    # for UpdateView the convention is <model>_form.html
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # checks if the user that wants to update the form is the user that created it
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user ==  post.author:
+            return True
+        return False
+
+# delete view
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    # template convention is <model>_confirm_delete.html
+    # checks if the user that wants to delete the form is the user that created it
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user ==  post.author:
+            return True
+        return False
+    success_url = '/'
+
+
+'''
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
-"""
-
+'''
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
